6fdd9041afffa492f4ef70c94b2134e0b51c2491
Utkarsh Goyal <utkarshgoyal04@gmail.com>
2026-05-29 00:23:00 +0530

    Day_2

---
+++ problem6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number,temp,sum=0;
+    cout<<"Enter the number : ";
+    cin>>number;
+    while(number>0){
+        temp=number%10;
+        sum=sum*10+temp;
+        number/=10;
+    }
+    cout<<"The reversed number is : "<<sum;
+    return 0;
+}
