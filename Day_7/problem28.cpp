c3bf70031a6d5d62814d19dac63ed24f7f06c2f3
Utkarsh Goyal <utkarshgoyal04@gmail.com>
2026-06-03 18:01:11 +0530

    commit

---
+++ Day_7/problem28.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int reverseNum(int n, int rev = 0)
+{
+    if(n == 0)
+        return rev;
+
+    return reverseNum(n / 10, rev * 10 + n % 10);
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Reversed number = " << reverseNum(n);
+
+    return 0;
+}
